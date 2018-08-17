deb2e95be60768896350f9fcc7046f005e7b1fa1
Reza Bayat <reza.bayat76@gmail.com>
2018-08-17 14:46:30 +0430

    [MASTER <UPDATE FILE>] -> get service name from args and run that

--- src/main/java/in/nimbo/isDoing/sparkJobs/Main.java
+++ src/main/java/in/nimbo/isDoing/sparkJobs/Main.java
@@ -1,4 +1,32 @@
 package in.nimbo.isDoing.sparkJobs;
 
+import in.nimbo.isDoing.sparkJobs.trending.TrendServiceManager;
+import in.nimbo.isDoing.sparkJobs.trending.TrendServiceManagerConfigs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class.getSimpleName());
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            logger.error("Invalid args");
+            System.exit(0);
+        }
+        String appName = args[0];
+
+        Configs trendServiceManagerConfigs = null;
+        TrendServiceManager trendServiceManager = null;
+        try {
+            trendServiceManagerConfigs = new TrendServiceManagerConfigs();
+            trendServiceManager = new TrendServiceManager(trendServiceManagerConfigs);
+        } catch (Exception e) {
+            logger.error("Problem when creating instance of TrendServiceManagerConfigs: ", e);
+            throw new IllegalStateException("roblem when creating instance of TrendServiceManagerConfigs");
+        }
+
+//        TrendServiceManager trendServiceManager = new TrendServiceManager(trendServiceManagerConfigs);
+        trendServiceManager.startService(appName);
+    }
+
 }
